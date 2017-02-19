c1daba1e7047dba8a4cc1ffbed384a93c021b4a5
Jayraj Jog <jayraj@skyhighnetworks.com>
2017-02-18 17:05:10 -0800

    Changing colors of nav

--- js/main.js
+++ js/main.js
@@ -25,4 +25,17 @@
       setAboutPageHeight();
     }, 250);
   });
+
+  $(window).on('activate.bs.scrollspy', function (evt, activated) {
+    // make nav bg green
+    $('nav').removeClass();
+    $('nav').addClass('navbar fixed-top navbar-toggleable-sm navbar-light bg-faded')
+    if (activated.relatedTarget === '#home') {
+
+    } else if (activated.relatedTarget === '#portfolio') {
+      $('nav').addClass('project-details');
+    } else if(activated.relatedTarget === '#about') {
+      $('nav').addClass('about-page-nav')
+    }
+  });
 }());
