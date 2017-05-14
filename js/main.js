4b51245b2bff4b5d7c8d7b10f0dd0cb7fcf647b7
Jayraj Jog <jogjayr@gmail.com>
2017-05-14 15:32:58 -0700

    Rework site into new design

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
   function setPortfolioPageHeight() {
     var windowHeight = $(window).height();
-    $('#portfolio .top-half').height(windowHeight / 2.6);
+    $('#portfolio .top-half').height('170px');
   }
 
   function setAboutPageHeight() {
@@ -28,11 +28,15 @@
   $(window).on('activate.bs.scrollspy', function (evt, activated) {
     // make nav bg green
     $('nav').removeClass();
+    $('#portfolio-top-half').removeClass('fixed-top-half');
+    $('#portfolio-bottom-half').removeClass('fixed-top-half');
+    
     $('nav').addClass('navbar fixed-top navbar-toggleable-sm navbar-light bg-faded')
     if (activated.relatedTarget === '#home') {
 
     } else if (activated.relatedTarget === '#portfolio') {
-      $('nav').addClass('project-details');
+      $('#portfolio-top-half').addClass('fixed-top-half');
+      $('#portfolio-bottom-half').addClass('fixed-top-half');
     } else if(activated.relatedTarget === '#about') {
       $('nav').addClass('about-page-nav')
     }
