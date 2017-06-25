0ec7a8fd5a5de55709810f7685fb0f469081f9e7
Jayraj Jog <jogjayr@gmail.com>
2017-06-25 14:03:52 -0700

    Add animation css for bg image
    Remove unused JS

--- js/main.js
+++ js/main.js
@@ -19,14 +19,10 @@
   });
 
   $(window).on('activate.bs.scrollspy', function (evt, activated) {
-    // make nav bg green
-    $('nav').removeClass();
     $('#portfolio-top-half').removeClass('fixed-top-half');
     $('#portfolio-bottom-half').removeClass('fixed-top-half');
     $('#about-top-half').removeClass('fixed-top-half');
     $('#about-bottom-half').removeClass('fixed-top-half');
-    
-    $('nav').addClass('navbar fixed-top navbar-toggleable-sm navbar-light bg-faded')
     if (activated.relatedTarget === '#home') {
 
     } else if (activated.relatedTarget === '#portfolio' || activated.relatedTarget === '#about') {
