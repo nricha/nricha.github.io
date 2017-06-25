a6b1e4452808048dabdabc5c1ab42fc1e632d48f
Jayraj Jog <jogjayr@gmail.com>
2017-06-25 15:13:42 -0700

    New bg image

--- js/main.js
+++ js/main.js
@@ -19,17 +19,17 @@
   });
 
   $(window).on('activate.bs.scrollspy', function (evt, activated) {
-    $('#portfolio-top-half').removeClass('fixed-top-half');
-    $('#portfolio-bottom-half').removeClass('fixed-top-half');
-    $('#about-top-half').removeClass('fixed-top-half');
-    $('#about-bottom-half').removeClass('fixed-top-half');
-    if (activated.relatedTarget === '#home') {
+    // $('#portfolio-top-half').removeClass('fixed-top-half');
+    // $('#portfolio-bottom-half').removeClass('fixed-top-half');
+    // $('#about-top-half').removeClass('fixed-top-half');
+    // $('#about-bottom-half').removeClass('fixed-top-half');
+    // if (activated.relatedTarget === '#home') {
 
-    } else if (activated.relatedTarget === '#portfolio' || activated.relatedTarget === '#about') {
-      $('#portfolio-top-half').addClass('fixed-top-half');
-      $('#portfolio-bottom-half').addClass('fixed-top-half');
-      $('#about-top-half').addClass('fixed-top-half');
-      $('#about-bottom-half').addClass('fixed-top-half');
-    }
+    // } else if (activated.relatedTarget === '#portfolio' || activated.relatedTarget === '#about') {
+    //   $('#portfolio-top-half').addClass('fixed-top-half');
+    //   $('#portfolio-bottom-half').addClass('fixed-top-half');
+    //   $('#about-top-half').addClass('fixed-top-half');
+    //   $('#about-bottom-half').addClass('fixed-top-half');
+    // }
   });
 }());
