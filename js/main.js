f68b79d1c62710c16912793ecb3036866115ad1c
Jayraj Jog <jogjayr@gmail.com>
2017-07-10 21:34:35 -0700

    Working now!

--- js/main.js
+++ js/main.js
@@ -17,4 +17,35 @@
       setAboutPageHeight();
     }, 250);
   });
+
+  $('button.navbar-toggler').click((evt) => {
+    const $currentTarget = $(evt.currentTarget);
+    let ariaExpanded = $currentTarget.attr('aria-expanded');
+    ariaExpanded = ariaExpanded === 'true';
+    $currentTarget.attr('aria-expanded', !ariaExpanded);
+    const $hamburger = $('#navbarNavAltMarkup');
+    $hamburger.toggleClass('show');
+
+    $('.nav-link').on('click', () => {
+      closeHamburger();
+      $('.nav-link').off('click');
+    });
+
+    $currentTarget.find('.close-icon').on('click', (event) => {
+      closeHamburger();
+      event.preventDefault();
+      event.stopPropagation();
+      $currentTarget.find('.close-icon').off('click');
+      return false;
+    });
+  });
+
+  function closeHamburger() {
+    const $menuToggler = $('button.navbar-toggler');
+    let ariaExpanded = $menuToggler.attr('aria-expanded');
+    ariaExpanded = ariaExpanded === 'true';
+    $menuToggler.attr('aria-expanded', !ariaExpanded);
+    const $hamburger = $('#navbarNavAltMarkup');
+    $hamburger.removeClass('show');
+  }
 }());
