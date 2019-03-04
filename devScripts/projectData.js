328f5d2809fe7d2a65873c08e4d9991006f9e083
Jayraj Jog <jogjayr@gmail.com>
2019-03-03 20:05:55 -0800

    Make different sections for projects
    Break projects into 3 sections, add styling per section

--- devScripts/projectData.js
+++ devScripts/projectData.js
@@ -1,102 +1,105 @@
-module.exports = [{
-  name: 'old-siam',
-  title: 'The Old Siam',
-  type: 'Restaurant re-branding',
-},
-{
-  name: 'atl-film-fest',
-  title: 'Atlanta Film Festival',
-  type: 'Direct Mail Kit'
-},
-{
-  name: 'get-fit',
-  title: 'Get Fit',
-  type: 'Mobile &amp; Web App',
-  thumbnailFormat: 'png',
-},
-{
-  name: 'picasso',
-  title: 'Picasso',
-  type: 'Museum Catalog'
-},
-{
-  name: 'cherry-blossom-fest',
-  title: 'Cherry Blossom Festival',
-  type: 'Branding &amp; Swag'
-},
-{
-  name: 'porsche',
-  title: 'Porsche',
-  type: 'Digital Illustration'
-},
-{
-  name: 'try-on-app',
-  title: 'Try On App',
-  type: 'Mobile App Design'
-},
-{
+module.exports = {
+  print: [{
+    name: 'old-siam',
+    title: 'The Old Siam',
+    type: 'Restaurant re-branding',
+  },
+  {
+    name: 'atl-film-fest',
+    title: 'Atlanta Film Festival',
+    type: 'Direct Mail Kit'
+  },
+  {
+    name: 'picasso',
+    title: 'Picasso',
+    type: 'Museum Catalog'
+  },
+  {
+    name: 'cherry-blossom-fest',
+    title: 'Cherry Blossom Festival',
+    type: 'Branding & Swag'
+  },
+  {
     name: 'collage',
     title: 'Editorial Collage',
     type: 'Poster Design',
-},
-{
-  name: 'turtle-website',
-  title: 'Save A Turtle',
-  type: 'Web Design',
-},
-{
-  name: 'turtle-styleguide',
-  title: 'Style Guide',
-  type: 'Branding &amp; Usage Guidelines',
-},
-{
-  name: 'paula-scher',
-  title: 'Paula Scher',
-  type: 'Illustration &amp; Catalog Design',
-},
-{
-  name: 'rihanna',
-  title: 'Rihanna',
-  type: 'Web Design',
-},
-{
-  name: 'knots-and-weaves',
-  title: 'Knots &amp; Weaves',
-  type: 'Web Design'
-},
-{
-  name: 'cross',
-  title: 'Cross Pens',
-  type: 'Brochure Design',
-},
-{
-  name: 'flowers',
-  title: 'Oriental Lily',
-  type: 'Digital Illustration',
-},
-{
-  name: 'horoscope',
-  title: 'Horoscope',
-  type: 'Tablet App Design',
-},
-{
-  name: 'sufi-cafe',
-  title: 'Sufi Café',
-  type: 'Branding &amp; Collateral'
-},
-{
-  name: 'art-and-illustrations',
-  title: 'Art &amp; Illustrations',
-  type: 'Sketches, Watercolors &amp; Pastels'
-},
-{
-  name: 'moreno-catalog',
-  title: 'Gabriel Moreno',
-  type: 'Museum Catalog'
-},
-{
-  name: 'skateboard',
-  title: 'Skateboard Graphics',
-  type: 'Digital Illustration',
-},
-];
+  },
+  {
+    name: 'turtle-styleguide',
+    title: 'Style Guide',
+    type: 'Branding & Usage Guidelines',
+  },
+  {
+    name: 'cross',
+    title: 'Cross Pens',
+    type: 'Brochure Design',
+  },
+  {
+    name: 'sufi-cafe',
+    title: 'Sufi Café',
+    type: 'Branding & Collateral'
+  },
+  {
+    name: 'moreno-catalog',
+    title: 'Gabriel Moreno',
+    type: 'Museum Catalog'
+  }],
+
+  illustrations: [{
+    name: 'porsche',
+    title: 'Porsche',
+    type: 'Digital Illustration'
+  },
+  {
+    name: 'paula-scher',
+    title: 'Paula Scher',
+    type: 'Illustration & Catalog Design',
+  },
+  {
+    name: 'flowers',
+    title: 'Oriental Lily',
+    type: 'Digital Illustration',
+  },
+  {
+    name: 'art-and-illustrations',
+    title: 'Art & Illustrations',
+    type: 'Sketches, Watercolors & Pastels'
+  },
+  {
+    name: 'skateboard',
+    title: 'Skateboard Graphics',
+    type: 'Digital Illustration',
+  }],
+
+  'ui-design': [{
+    name: 'get-fit',
+    title: 'Get Fit',
+    type: 'Mobile & Web App',
+    thumbnailFormat: 'png',
+  },
+  {
+    name: 'try-on-app',
+    title: 'Try On App',
+    type: 'Mobile App Design'
+  },
+  {
+    name: 'turtle-website',
+    title: 'Save A Turtle',
+    type: 'Web Design',
+  },
+  {
+    name: 'rihanna',
+    title: 'Rihanna',
+    type: 'Web Design',
+  },
+  {
+    name: 'knots-and-weaves',
+    title: 'Knots & Weaves',
+    type: 'Web Design'
+  },
+  {
+    name: 'horoscope',
+    title: 'Horoscope',
+    type: 'Tablet App Design',
+  }]
+};
