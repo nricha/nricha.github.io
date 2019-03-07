f98075b1562d06dbbe0bbc5f02bda4f91d7bf883
Jayraj Jog <jogjayr@gmail.com>
2019-03-06 23:54:31 -0800

    Many fixes
    1. Add alt text for all the thumbnail images
    2. Add "About me" title

--- devScripts/projectData.js
+++ devScripts/projectData.js
@@ -3,108 +3,129 @@ module.exports = {
     name: 'old-siam',
     title: 'The Old Siam',
     type: 'Restaurant re-branding',
+    thumbnailAltText: 'Thumbnail - Shows a redesigned coaster for The Old Siam restaurant.',
   },
   {
     name: 'atl-film-fest',
     title: 'Atlanta Film Festival',
-    type: 'Direct Mail Kit'
+    type: 'Direct Mail Kit',
+    thumbnailAltText: 'Thumbnail with all the swag for the film fest. Envelopes, lanyard, t-shirt, tickets, letterhead, and parking pass.',
   },
   {
     name: 'picasso',
     title: 'Picasso',
-    type: 'Museum Catalog'
+    type: 'Museum Catalog',
+    thumbnailAltText: 'Thumbnail - Cover of the Pablo Picasso museum catalog.',
   },
   {
     name: 'cherry-blossom-fest',
     title: 'Cherry Blossom Festival',
-    type: 'Branding & Swag'
+    type: 'Branding & Swag',
+    thumbnailAltText: 'Thumbnail - Mockup of the award-winning poster I designed for the Cherry Blossom Festival.',
   },
   {
     name: 'collage',
     title: 'Editorial Collage',
     type: 'Poster Design',
+    thumbnailAltText: 'Thumbnail - Mockup of a collage of the Plastic Ocean poster.',
   },
   {
     name: 'turtle-styleguide',
     title: 'Style Guide',
     type: 'Branding & Usage Guidelines',
+    thumbnailAltText: 'Thumbnail - Styleguide for a turtle conservation non-profit.',
   },
   {
     name: 'cross',
     title: 'Cross Pens',
     type: 'Brochure Design',
+    thumbnailAltText: 'Thumbnail - Redesigned Cross pens brochure.',
   },
   {
     name: 'sufi-cafe',
     title: 'Sufi Café',
-    type: 'Branding & Collateral'
+    type: 'Branding & Collateral',
+    thumbnailAltText: 'Thumbnail - Mockup of a store sign for Sufi Café.',
   },
   {
     name: 'moreno-catalog',
     title: 'Gabriel Moreno',
-    type: 'Museum Catalog'
+    type: 'Museum Catalog',
+    thumbnailAltText: 'Thumbnail - Catalog for Gabriel Moreno',
   }],
 
   illustrations: [{
     name: 'porsche',
     title: 'Porsche',
-    type: 'Digital Illustration'
+    type: 'Digital Illustration',
+    thumbnailAltText: 'Thumbnail - Mockup of my digital illustration of a Porsche on a poster.',
   },
   {
     name: 'paula-scher',
     title: 'Paula Scher',
     type: 'Illustration & Catalog Design',
+    thumbnailAltText: 'Thumbnail - Mockup of Paula Scher catalog',
   },
   {
     name: 'flowers',
     title: 'Oriental Lily',
     type: 'Digital Illustration',
+    thumbnailAltText: 'Thumbnail - Mockup of my illustration of orientatl lilies.'
   },
   {
     name: 'art-and-illustrations',
     title: 'Art & Illustrations',
-    type: 'Sketches, Watercolors & Pastels'
+    type: 'Sketches, Watercolors & Pastels',
+    thumbnailAltText: 'Thumbnail - My copy of van Gogh\'s Room',
   },
   {
     name: 'skateboard',
     title: 'Skateboard Graphics',
     type: 'Digital Illustration',
+    thumbnailAltText: 'Thumbnail - Mockup of illustrations on skateboards',
   }],
 
   'ui-design': [{
     name: 'munchkin',
     title: 'Munchkin - Kids\' Recipe App',
     type: 'Mobile App',
+    thumbnailAltText: 'Thumbnail - Mockups of the Munckin app on phones',
   },
   {
     name: 'get-fit',
     title: 'Get Fit',
     type: 'Mobile & Web App',
     thumbnailFormat: 'png',
+    thumbnailAltText: 'Thumbnail - Text logo reading GetFit',
   },
   {
     name: 'try-on-app',
     title: 'Try On App',
-    type: 'Mobile App Design'
+    type: 'Mobile App Design',
+    thumbnailAltText: 'Thumbnail - Mockup of the Try On app on a tablet.',
   },
   {
     name: 'turtle-website',
     title: 'Save A Turtle',
     type: 'Web Design',
+    thumbnailAltText: 'Thumbnail - Mockup of my re-designed Save a Turtle website on a Mac.',
   },
   {
     name: 'rihanna',
     title: 'Rihanna',
     type: 'Web Design',
+    thumbnailAltText: 'Thumbnail - Mockup of my re-designed Rihanna website on a Macbook.',
   },
   {
     name: 'knots-and-weaves',
     title: 'Knots & Weaves',
-    type: 'Web Design'
+    type: 'Web Design',
+    thumbnailAltText: 'Thumbnail - Mockup of a website for Knots-and-Weaves carpet design on desktop, laptop, and phone.',
   },
   {
     name: 'horoscope',
     title: 'Horoscope',
     type: 'Tablet App Design',
+    thumbnailAltText: 'Thumbnail - Mockup of Horoscope app on an iPad.',
   }]
 };
